790d69a62ee9de6674b9929b2e6232412d8b1b41
Kuba Szóstko <kubaszostko@gmail.com>
2019-07-08 10:51:55 +0200

    updated main.js

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,27 @@
 import moment from 'moment';
 
 console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
+
+console.log("siema");
+
+const name = "Kuba";
+let age = 20;
+
+console.log(`Nazywam sie ${name} i mam ${age} lat`);
+
+const description = document.querySelector('.about__description--js');
+description.innerHTML = '<strong>Cześć</strong> JS';
+
+const hello = (firstName, lastName) => {
+    console.log(`Witaj ${firstName} ${lastName}, miło cię widzieć!`);
+}
+
+hello('Kuba', 'Szóstko');
+
+// hamburger menu
+const menuList = document.querySelector('.menu__list--js');
+const listButton = document.querySelector('.menu__button--js');
+
+listButton.addEventListener('click', () => {
+    menuList.classList.toggle('menu__list--visible');
+})
